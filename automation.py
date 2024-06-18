91a0d42c0594eda4973b0818799fb9e902438d37
Rishav Jha <100018260+jharishav99@users.noreply.github.com>
2024-06-18 20:47:03 +0545

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time 
+
+time.sleep(0.5)
+for i in range(1):
+   pyautogui.click(x=1074, y=1048)
+   time.sleep(1)
+   pyautogui.click(x=1084, y=476)
+   time.sleep(1)
+   pyautogui.click(x=1015, y=122)
+   time.sleep(1)
+   pyautogui.click(x=1406, y=399)
+   time.sleep(1)
